ff6af762e2751398f76dbb197407afea69ad035e
Martin Ortiz <martinortizpalacio@gmail.com>
2020-02-08 23:18:53 -0300

    ADD: files from python course

---
+++ example_init.py
@@ -0,0 +1,13 @@
+import turtle
+
+window = turtle.Screen()
+victor = turtle.Turtle()
+victor.forward(50)
+victor.left(90)
+victor.forward(50)
+victor.left(90)
+victor.forward(50)
+victor.left(90)
+victor.forward(50)
+victor.left(90)
+turtle.mainloop()
---
+++ test.py
@@ -0,0 +1,4 @@
+
+name = str(input("Cuál es tu nombre? "))
+
+print('Hola, {name}!'.format(name=name))
