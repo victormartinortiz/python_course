56f99b4fd8c4b7b18f167fcf2b7ee8415113adc4
Martin Ortiz <martinortizpalacio@gmail.com>
2020-03-02 16:19:44 -0300

    FIX: Fix typo __main___ in main.py

--- server/main.py
+++ server/main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-	return 'Hello, software engineer graduated from Platzi!'
+    return render_template('contact_book.html')
 
 
-if __name__ == "__main___":
-	app.run()
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080, debug=True)
